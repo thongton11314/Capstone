47c425c68edbe8169d4501b1876914803f61df9a
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2022-01-29 14:58:55 -0800

    Update

--- capstone_venv/source/lambdaSearch.py
+++ capstone_venv/source/lambdaSearch.py
@@ -14,22 +14,22 @@ def lambda_search(event, context):
     
     ##### check valid context #####
     if (len(event) != 3):
-        return toRestAPI(status=200, body=json.dumps('Invalid parameters. Exactly 3 parameters'));
+        return toRestAPI(status=200, body=json.dumps('Invalid parameters. Exactly 3 parameters'))
     
     # Valid context
     else:
         
         # Check if valid zipcode string
         if (len(str(event['zipcode'])) > 6):
-            return toRestAPI(status=200, body=json.dumps('Zipcode is out of range. Only 5 digits'));
+            return toRestAPI(status=200, body=json.dumps('Zipcode is out of range. Only 5 digits'))
         
         # Check if valid radius
         if (event['radius'] > 100):
-            return toRestAPI(status=200, body=json.dumps('Radius should be less than 100'));
+            return toRestAPI(status=200, body=json.dumps('Radius should be less than 100'))
         
         # Check if valid items string
         if (not event['item'] or event['item'].isspace()):
-            return toRestAPI(status=200, body=json.dumps('Input item invalid'));
+            return toRestAPI(status=200, body=json.dumps('Input item invalid'))
     
     ##### Start searching #####
     k = Kroger()
