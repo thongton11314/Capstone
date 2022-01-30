96f8768854b8dbcbb36c450a852e4890f7c72299
thongton11314 <46808153+thongton11314@users.noreply.github.com>
2022-01-29 16:35:08 -0800

    Intergrate with AWS API successful

--- capstone_venv/source/lambdaSearch.py
+++ capstone_venv/source/lambdaSearch.py
@@ -12,32 +12,38 @@ def toRestAPI(status, body, headers=None):
 # event is JSON object that map in from query parameters
 def lambda_search(event, context):
     
-    ##### check valid context #####
-    if (len(event) != 3):
-        return toRestAPI(status=200, body=json.dumps('Invalid parameters. Exactly 3 parameters'))
+    # Check if contains zipcode, radius, item name
+    if (len(event['queryStringParameters']) != 3):
+        return toRestAPI(status=200, body=json.dumps('Must input: zipcode, radius, item'))
     
-    # Valid context
-    else:
+    # Mapping data
+    data = {
+        'zipcode' : int(event['queryStringParameters']['zipcode']),
+        'radius' : int(event['queryStringParameters']['radius']),
+        'item' : event['queryStringParameters']['item']
+    }
+    
+    ##### check valid event(JSON) #####
+    # Check if valid zipcode string
+    if (len(str(data['zipcode'])) > 6):
+        return toRestAPI(status=200, body=json.dumps('Zipcode is out of range. Only 5 digits'))
         
-        # Check if valid zipcode string
-        if (len(str(event['zipcode'])) > 6):
-            return toRestAPI(status=200, body=json.dumps('Zipcode is out of range. Only 5 digits'))
+    # Check if valid radius
+    if (int(data['radius']) > 100):
+        return toRestAPI(status=200, body=json.dumps('Radius should be less than 100'))
         
-        # Check if valid radius
-        if (event['radius'] > 100):
-            return toRestAPI(status=200, body=json.dumps('Radius should be less than 100'))
-        
-        # Check if valid items string
-        if (not event['item'] or event['item'].isspace()):
-            return toRestAPI(status=200, body=json.dumps('Input item invalid'))
+    # Check if valid items string
+    if (not data['item'] or data['item'].isspace()):
+        return toRestAPI(status=200, body=json.dumps('Input item invalid'))
+
     
     ##### Start searching #####
     k = Kroger()
-    items = k.search(event['zipcode'], event['radius'], event['item'])
+    items = k.search(data['zipcode'], data['radius'], data['item'])
     
     # Found Nothing
     if (len(items) <= 0):
-        return toRestAPI(status=200, body=json.dumps('[]'))
+        return toRestAPI(status=200, body=json.dumps('Item not found!'))
     
     # Found items
-    return  toRestAPI(status=200, body=items)
+    return  toRestAPI(status=200, body=json.dumps(items))
