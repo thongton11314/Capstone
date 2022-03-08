930d8fc60392550bba90a7990f5c9ccdbb2cb787
Huy <49509023+huyn8@users.noreply.github.com>
2022-03-07 20:53:22 -0800

    upload front + backend code

---
+++ main-program/Models/DisplayModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+/**
+* 
+* 
+* 
+* 
+*/
+namespace program.Models
+{
+    public class DisplayModel
+    {
+        public Dictionary<string, double> itemsInfo { get; set; }
+        public string storeAddress { get; set; }
+        public string storeName { get; set; }
+
+    }
+}
